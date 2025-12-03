0ee6ce7ce0e3d624975243ea3c90e4911790dc57
KhangPham205 <ptkhang17122005@gmail.com>
2025-12-03 16:54:14 +0700

    Init Skeleton Project

---
+++ FacilityManagementServer/FacilityManagementServer/Repositories/Interfaces/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacilityManagementServer.Repositories.Implements;
+
+namespace FacilityManagementServer.Repositories.Interfaces
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+    }
+}
