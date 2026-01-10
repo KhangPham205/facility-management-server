63da64dddf94bcaec4aa6c4cf73103d8c82ef572
baobui13 <23520093@gm.uit.edu.vn>
2026-01-10 22:26:11 +0700

    create PaginationFilter

---
+++ backend/backend/vo/PaginationFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.vo
+{
+    public class PaginationFilter
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SortBy { get; set; }
+        public bool IsDescending { get; set; } = true;
+    }
+}
