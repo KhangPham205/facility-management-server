95437a71218e563c0761adddb1695e5189e4fc31
baobui13 <23520093@gm.uit.edu.vn>
2026-01-13 15:29:31 +0700

    fix updateBuilding data annotation

--- backend/backend/DTOs/Building/Request/UpdateBuildingRequest.cs
+++ backend/backend/DTOs/Building/Request/UpdateBuildingRequest.cs
@@ -4,11 +4,9 @@ namespace backend.DTOs.Building.Request
 {
     public class UpdateBuildingRequest
     {
-        [Required]
         [StringLength(100)]
         public string? BuildingName { get; set; }
 
-        [Required]
         [Range(0, 100)]
         public int FloorCount { get; set; }
 
