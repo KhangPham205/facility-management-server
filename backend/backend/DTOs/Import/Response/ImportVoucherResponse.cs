837aaae23bf1b5bf23c6109a91413d5ff9f40abe
KhangPham205 <ptkhang17122005@gmail.com>
2026-01-14 00:00:16 +0700

    Update ImportVoucherResponse.cs

--- backend/backend/DTOs/Import/Response/ImportVoucherResponse.cs
+++ backend/backend/DTOs/Import/Response/ImportVoucherResponse.cs
@@ -8,6 +8,7 @@
         public string InvoiceNumber { get; set; }
         public decimal TotalAmount { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
         public List<ImportVoucherDetailResponse> Details { get; set; }
     }
 }
