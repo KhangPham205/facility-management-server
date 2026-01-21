d5dddf222fffc535c1789880bc2ac4577636c117
baobui13 <23520093@gm.uit.edu.vn>
2026-01-21 22:18:25 +0700

    remove id from audit detail controller

--- backend/backend/Controllers/AuditDetailController.cs
+++ backend/backend/Controllers/AuditDetailController.cs
@@ -41,7 +41,7 @@ namespace backend.Controllers
             }
         }
 
-        [HttpGet("{id}")]
+        [HttpGet]
         public async Task<ActionResult<AuditDetailResponse>> GetById(string auditId, string equipmentId)
         {
             var result = await _service.GetById(auditId, equipmentId);
@@ -63,7 +63,7 @@ namespace backend.Controllers
         //    }
         //}
 
-        [HttpPut("{id}")]
+        [HttpPut]
         public async Task<ActionResult<AuditDetailResponse>> Update(string auditId, string equipmentId, [FromBody] UpdateAuditDetailRequest request)
         {
             try
@@ -81,7 +81,7 @@ namespace backend.Controllers
             }
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete]
         public async Task<IActionResult> Delete(string auditId, string equipmentId)
         {
             try
